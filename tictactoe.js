9baa040685a1a73399350faa61a4ea11466983e7
aarondeane <aaronmdeane@gmail.com>
2019-10-03 19:49:33 -0700

    added game logic to check for winner

--- tictactoe.js
+++ tictactoe.js
@@ -15,6 +15,8 @@ const board = {
   9: '9',
 }
 
+let totalPlays = 0;
+
 function printBoard() {
   console.log(`
     ${board[1]} | ${board[2]} | ${board[3]}
@@ -33,18 +35,47 @@ function validateTurn(position) {
   return (board[position] !== 'X' && board[position] !=='O');
 }
 
+function checkWin(player, position) {
+  let marks = 1;
+  const winningCombos = {
+    1: [[2,3], [4,7], [5,9]],
+    2: [[1,3], [5,8]],
+    3: [[1,2], [6,9], [5,7]],
+    4: [[5,6], [1,7]],
+    5: [[4,6], [2,8], [1,9], [3,7]],
+    6: [[4,5], [3,9]],
+    7: [[8,9], [1,4], [3,5]],
+    8: [[7,9], [2,5]],
+    9: [[1,5], [3,6], [7,8]],
+  }
+  for (let combo of winningCombos[position]) {
+    for (let pos of combo) {
+      if (board[pos] === player) {
+        marks+=1
+      }
+    }
+    if (marks === 3) return true;
+    marks = 1;
+  }  
+}
+
+
 function playTurn(player) {
+  totalPlays+=1;
   readline.question(`Your turn ${player}, enter a position -> `, (position) => {
     if (validateTurn(position)) {
       markBoard(position, player);
     } else {
-      console.log('Sorry that is not a valid play, please try again');
+      console.log('Sorry that is not a valid play');
       playTurn(player);
     }
-    // Check for a win or game over
-    // call for the other players turn
 
-    // readline.close();
+    if (totalPlays > 2 && checkWin(player, position) === true) {
+      printBoard();
+      console.log(`Congrats! Player ${player} has won!`);
+      readline.close();
+      return;
+    }
     printBoard();
     player === 'X' ? playTurn('O') : playTurn('X');
   })
